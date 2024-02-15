2c0edf93b894163b42123090d6daf797e87c4724
Yummyzinn <enricozagallo92@gmail.com>
2024-02-15 18:45:21 -0300

    cronometro prototype

--- index.js
+++ index.js
@@ -1,5 +1,75 @@
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('select').onchange = function() {
             document.querySelector('#hello').style.color    = this.value
     }
 })
+let start;
+
+document.getElementById('parar').addEventListener('click',stop, );
+document.getElementById('parar').addEventListener('click',stopa, );
+document.getElementById('parar').addEventListener('click',stopb, );
+document.getElementById('iniciar').addEventListener('click', all,);
+document.getElementById('iniciar').addEventListener('click', alla,);
+document.getElementById('iniciar').addEventListener('click', allb,);
+function all() {
+  let sec = 0
+    function segundos() {
+        if (sec>=10){
+        document.getElementById('sec').innerHTML = sec++  } 
+        else {
+            document.getElementById('sec').innerHTML = '0' + sec++
+        }
+    }
+intervalID = window.setInterval(segundos, 1000);
+    
+}
+function stop(){ 
+clearInterval(intervalID)
+
+}
+
+
+
+
+
+
+function alla() {
+  let min = 0
+    function minutos() {
+        if (min>=10){
+        document.getElementById('min').innerHTML = min++  } 
+        else {
+            document.getElementById('min').innerHTML = '0' + min++
+        }
+        
+    }
+intervalIDa = window.setInterval(minutos, 60000);
+    
+}
+function stopa(){ 
+clearInterval(intervalIDa)
+
+}
+
+
+
+
+
+
+function allb() {
+  let hora = 0
+    function horario() {
+        if (hora>=10){
+        document.getElementById('hora').innerHTML = hora++  } 
+        else {
+            document.getElementById('hora').innerHTML = '0' + hora++
+        }
+    }
+intervalIDb = window.setInterval(horario, 3600000);
+    
+}
+function stopb(){ 
+clearInterval(intervalIDb)
+
+}
+
